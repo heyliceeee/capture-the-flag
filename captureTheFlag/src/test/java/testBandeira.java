e95cec407728aef0dc59d240c6cb94b28885644a
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2024-01-22 17:34:18 +0000

    🧪 test: getters e setters bandeira

--- captureTheFlag/src/test/java/testBandeira.java
+++ captureTheFlag/src/test/java/testBandeira.java
@@ -31,4 +31,30 @@ public class testBandeira
     {
         assertThrows(IllegalArgumentException.class, () -> { IBandeira bandeira = new Bandeira(0, null, null, null); }); //retorna erro porque foi criado contrutor sem sucesso
     }
+
+
+    @DisplayName("Getters e Setters da Bandeira")
+    @Test
+    public void testGettersSettersBandeira_Valid()
+    {
+        IBandeira bandeira = new Bandeira(0, "Bandeira", "Bandeira do Jogador 1", new Coordenada(0.0, 0.0));
+
+        assertEquals(0, bandeira.getId()); //se o id == 0
+        bandeira.setId(1);
+        assertEquals(1, bandeira.getId()); //se o id == 1
+
+        assertEquals("Bandeira", bandeira.getTipo()); //se o tipo == "Bandeira"
+        bandeira.setTipo("Localizacao");
+        assertEquals("Localizacao", bandeira.getTipo()); //se o tipo == "Localizacao"
+
+        assertEquals("Bandeira do Jogador 1", bandeira.getNome()); //se o nome == "Bandeira do Jogador 1"
+        bandeira.setNome("Bandeira do Jogador 2");
+        assertEquals("Bandeira do Jogador 2", bandeira.getNome()); //se o nome == "Bandeira do Jogador 2"
+
+        assertEquals(0.0, bandeira.getCoordenadas().getLatitude()); //se o coordenadas.latitude == 0.0
+        assertEquals(0.0, bandeira.getCoordenadas().getLongitude()); //se o coordenadas.longitude == 0.0
+        bandeira.setCoordenadas(new Coordenada(1.0, 1.0));
+        assertEquals(1.0, bandeira.getCoordenadas().getLatitude()); //se o coordenadas.latitude == 1.0
+        assertEquals(1.0, bandeira.getCoordenadas().getLongitude()); //se o coordenadas.longitude == 1.0
+    }
 }
