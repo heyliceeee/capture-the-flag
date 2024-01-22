7859c79b1c24e46cccc06d670886648cf4b97be2
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2024-01-22 14:05:22 +0000

    🧪 test: contrutor correto bandeira

--- captureTheFlag/src/test/java/testBandeira.java
+++ captureTheFlag/src/test/java/testBandeira.java
@@ -1,2 +1,27 @@
-public class testBandeira {
+import org.example.api.implementation.Bandeira;
+import org.example.api.implementation.Coordenada;
+import org.example.api.interfaces.IBandeira;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testBandeira
+{
+    @DisplayName("Construtor correto da Bandeira")
+    @Test
+    public void testConstrutorCorretoBandeira_Valid()
+    {
+        assertDoesNotThrow(() -> { IBandeira bandeira = new Bandeira(0, "Bandeira", "Bandeira do Jogador 1", new Coordenada(0.0, 0.0)); }); //nao retorna nada porque foi criado contrutor com sucesso
+
+        IBandeira bandeira = new Bandeira(0, "Bandeira", "Bandeira do Jogador 1", new Coordenada(0.0, 0.0));
+
+        assertEquals(0, bandeira.getId()); //se o id == 6
+        assertEquals("Bandeira", bandeira.getTipo()); //se o tipo == "Bandeira"
+        assertEquals("Bandeira do Jogador 1", bandeira.getNome()); //se o nome == "Bandeira do Jogador 1"
+
+        assertEquals(0.0, bandeira.getCoordenadas().getLatitude()); //se o coordenadas.latitude == 0.0, 0.0
+        assertEquals(0.0, bandeira.getCoordenadas().getLongitude()); //se o coordenadas.longitude == 0.0, 0.0
+    }
 }
